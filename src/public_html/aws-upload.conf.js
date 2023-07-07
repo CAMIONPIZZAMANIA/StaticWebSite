9c56f5e6b5bf9d4c9a172d5176959d420bd21cf8
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-07-07 13:38:02 +0200

    chore:clean files to match with currenct prod

--- src/public_html/aws-upload.conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    credentials:"aws-credentials.json",
-    bucketName:"camionpizzamania.ch",
-    patterns:[
-        "index.html",
-        "components.html",
-        "assets/**"
-    ]
-}
