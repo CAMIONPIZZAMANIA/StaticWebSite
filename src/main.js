2cacdcb2594474a3842b74bb656b1708631bcae4
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2024-07-20 22:23:25 +0200

    feat: initial setup for pinia

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import './assets/css/pigga.css'
 import './assets/css/custom.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia)
+app.mount('#app')
