b167632bf74a50da691205c75ad84dfb13b945b8
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2024-07-21 14:56:51 +0200

    feature: cart is correctly set to run with watch

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import './assets/css/pigga.css'
 import './assets/css/custom.css'
 
-import { createApp } from 'vue'
+import { createApp, watch } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const pinia = createPinia();
 const app = createApp(App);
+
+watch(
+    pinia.state,
+    (state) => {
+        localStorage.setItem("cart", JSON.stringify(state.cart));
+    },
+    { deep: true }
+);
 
 app.use(pinia)
 app.mount('#app')
--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -2,9 +2,11 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
     state: () => {
+        if (localStorage.getItem("cart"))
+            return JSON.parse(localStorage.getItem("cart"));
         return {
             items: []
-        };
+        }
     },
     actions:{
         addItem(name:string, price:number, quantity:number){
