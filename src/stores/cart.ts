8531c604f41e4f801ed13996a636206015355c24
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2024-07-21 13:34:32 +0200

    fix: back to solution after frist watch implementation attempt

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,19 +1,9 @@
 import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
-    state: () => ({
-        count: 0,
-        total: 0.00,
-        items:[]
-    }),
-    getters:{
-        getCount:(state)=> state.count,
-        getTotal:(state)=> state.total,
-        getItems:(state)=> state.items
+    state: () => {
+        return {
+            items: []
+        };
     },
-    actions:{
-        addItem(name:string, price:string, quantity:number) {
-            this.items.push({ name, price, quantity })
-        }
-    }
 })
